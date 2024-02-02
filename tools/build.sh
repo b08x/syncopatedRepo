f8189ba605fd860d7fc44d1b6476baded5219222
b08x <rwpannick@gmail.com>
2024-02-02 02:14:52 -0500

    refactored build.sh

--- tools/build.sh
+++ tools/build.sh
@@ -24,16 +24,26 @@ say () {
 #########################################################################
 #                             set locations                             #
 #########################################################################
-declare -rx WORKSPACE="${HOME}/Workspace/syncopatedRepo"
+declare -rx SOUDNDBOT="/mnt/soundbot"
+declare -rx BUILDS="/srv/builds"
+
+declare -rx WORKSPACE="${SOUDNDBOT}/Workspace/syncopatedRepo"
 declare -rx PKGBUILDS="${WORKSPACE}/pkgbuilds"
-declare -rx SRCDEST="${WORKSPACE}/builds/packages/sources"
-declare -rx REPOSITORY_LOCAL="${WORKSPACE}/builds/repository"
-#########################################################################
-#                             set functions                             #
-#########################################################################
+
+declare -rx REPOSITORY_LOCAL="$BUILDS/repository"
+declare -rx REPOCTL_CONFIG="${WORKSPACE}/repoctl/config.toml"
+
+declare -rx MIRRORS=("bender.syncopated.net" "ec2-user@syncopated.hopto.org")
+
+declare -rx SRCDEST=$BUILDS/packages/sources
 
 declare -rx CHROOT="/mnt/chroots/arch"
 
+declare -rx PACKAGER="Robert Pannick <rwpannick@gmail.com>"
+declare -rx GPGKEY="DF7A6571781ACB52FA9CF8C1EB4DFE46828DFEDD"
+#########################################################################
+#                             set functions                             #
+#########################################################################
 unmount_chroot() {
   if mountpoint -q $CHROOT; then sudo umount $CHROOT; fi
 }
@@ -45,8 +55,9 @@ mount_chroot() {
 
 set_configs () {
   sudo chown -R $USER:$USER /usr/share/devtools
-  rsync -avP --delete $WORKSPACE/makepkg.conf.d/ /usr/share/devtools/makepkg.conf.d/
-  rsync -avP $WORKSPACE/pacman.conf.d/ /usr/share/devtools/pacman.conf.d/
+  rsync -a --delete $WORKSPACE/makepkg.conf.d/ /usr/share/devtools/makepkg.conf.d/
+  rsync -a $WORKSPACE/pacman.conf.d/ /usr/share/devtools/pacman.conf.d/
+  cp -f $REPOCTL_CONFIG $HOME/.config/repoctl/config.toml
 }
 
 cleanup() {
@@ -65,10 +76,6 @@ unmount_chroot
 #########################################################################
 #                             Greetings                                 #
 #########################################################################
-
-declare -rx GPG="DF7A6571781ACB52FA9CF8C1EB4DFE46828DFEDD"
-declare -rx MIRRORS=("bender.syncopated.net" "ec2-user@syncopated.hopto.org")
-
 gum style \
   --foreground 014 --border-foreground 024 --border double \
   --align center --width 50 --margin "1 2" --padding "2 4" \
@@ -83,24 +90,13 @@ say "-------------------------" $GREEN
 
 if [[ "${1}" == 'all' ]]; then
   package_selection=$(fd . -t d --max-depth=1 $PKGBUILDS -x echo -e {/})
-elif [[ "${1}" == 'package' ]]; then
+elif [[ "${1}" == '-p' ]]; then
   package_selection=$2
 else
   package_selection=$(fd . -t d --max-depth=1 $PKGBUILDS -x echo -e {/} | gum filter --no-limit )
 fi
 
 say "${package_selection}" $BLUE
-
-#########################################################################
-
-say "Select cpu architecture" $GREEN
-say "------------------------" $GREEN
-
-architectures=$(gum choose --no-limit all "x86_64" "x86_64_v3")
-
-if [[ $architectures == 'all' ]]; then
-  architectures=("x86_64" "x86_64_v3")
-fi
 
 #########################################################################
 trap cleanup SIGINT SIGTERM ERR EXIT
@@ -112,49 +108,69 @@ fi
 
 phrase=$(gum input --password)
 
-for arch in ${architectures[@]}; do
+mkdir -pv $SRCDEST
+mkdir -pv $BUILDS/{packages,repository}
 
-  if [[ $arch == 'x86_64' ]]; then
-    declare -x repo_db="syncopated.db.tar.zst"
-  elif [[ $arch == 'x86_64_v3' ]]; then
-    declare -x repo_db="syncopated-v3.db.tar.zst"
+
+for pkgname in ${package_selection[@]}; do
+
+  mount_chroot
+
+  cd "${PKGBUILDS}/${pkgname}"
+
+  updpkgsums && makepkg --printsrcinfo > .SRCINFO
+
+  srcinfo=($(grep -E '^\s*arch\s*=' .SRCINFO | grep -v 'i686' | choose 2 | grep -E 'x86_64|any'))
+
+  # Check if the arch is "any" and assign the array accordingly
+  if [ "$srcinfo" = "any" ]; then
+      architectures=("x86_64" "x86_64_v3")
+  else
+      architectures=("$srcinfo")
   fi
 
-  mkdir -pv $SRCDEST
+  echo $architectures
 
-  export PKGDEST=$WORKSPACE/builds/packages/$arch
-  export SRCPKGDEST=$WORKSPACE/builds/packages/srcpackages/$arch
-  export LOGDEST=$WORKSPACE/builds/packages/makepkglogs/$arch
+  for arch in ${architectures[@]}; do
 
-  for pkgname in ${package_selection[@]}; do
+    declare -x PKGDEST=$BUILDS/packages/$arch
+    declare -x SRCPKGDEST=$BUILDS/packages/srcpackages/$arch
+    declare -x LOGDEST=$BUILDS/packages/makepkglogs/$arch
 
-    mount_chroot
+    mkdir -p $PKGDEST
+    mkdir -p $SRCPKGDEST
+    mkdir -p $LOGDEST
 
-    cd "${PKGBUILDS}/${pkgname}"
+    extra-${arch}-build -c -r $CHROOT -- -c -n -u || continue
 
-    updpkgsums && makepkg --printsrcinfo > .SRCINFO
-
-    extra-${arch}-build -c -r $CHROOT -- -c -n -u
-
-    cd $WORKSPACE/builds/packages/$arch
+    cd $PKGDEST
 
     for pkg in *.zst; do
       echo "${phrase}" | gpg2 -v --batch --yes --detach-sign --pinentry-mode loopback --passphrase --passphrase-fd 0 --out $pkg.sig --sign $pkg
       repoctl add -P $arch -m -r ./$pkg
     done
 
-    cd $WORKSPACE/builds/repository/$arch
+    cd $BUILDS/repository/$arch
 
-    repo-add -n -k $GPG $repo_db *.pkg.tar.zst -s
+    if [[ $arch == 'x86_64' ]]; then
+      declare -x repo_db="syncopated.db.tar.zst"
+    elif [[ $arch == 'x86_64_v3' ]]; then
+      declare -x repo_db="syncopated-v3.db.tar.zst"
+    fi
+
+    repo-add -n -k $GPGKEY $repo_db *.pkg.tar.zst -s
+
+    cd "${PKGBUILDS}/${pkgname}"
   done
 
 done
 
 for mirror in ${MIRRORS[@]}; do
   echo -e "syncing local repository to remote ${mirror}\n"
-  rsync -avP --delete  "${WORKSPACE}/builds/repository/${arch}/" "${mirror}:/usr/share/nginx/html/syncopated/repo/${arch}/" || continue
+  rsync -avP --delete  "${BUILDS}/repository/" "${mirror}:/usr/share/nginx/html/syncopated/repo/" || continue
 done
 
-#gum confirm "deploy package(s) to repository?" && . deploy.sh
-
-# end of program
+gum style \
+  --foreground 014 --border-foreground 024 --border double \
+  --align center --width 50 --margin "1 2" --padding "2 4" \
+  'Enf of Program.' && sleep 1 && clear
--- tools/pkglist.sh
+++ tools/pkglist.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
 declare -rx WORKSPACE="${HOME}/Workspace"
-declare -rx PKGBUILDS="${WORKSPACE}/pkgrr/pkgbuilds"
+declare -rx PKGBUILDS="${WORKSPACE}/syncopatedRepo/pkgbuilds"
 
 declare -a package_list=$(fd . -t d --max-depth 1 $PKGBUILDS)
 
-
-
 for pkg in ${package_list[@]}; do
-  cd $pkg
+	cd $pkg
 
-  name=$(cat .SRCINFO | grep pkgbase | awk -F '=' '{ print $2 }' | xargs)
-  url=$(cat .SRCINFO | grep -w url | awk -F '=' '{ print $2 }' | xargs)
-  desc=$(cat .SRCINFO | grep pkgdesc | awk -F '=' '{ print $2 }' | xargs)
-  ver=$(cat .SRCINFO | grep pkgver | awk -F '=' '{ print $2 }' | xargs)
+	name=$(cat .SRCINFO | grep -E '^\s*pkgbase\s*=' | choose 2 | xargs -0)
+	url=$(cat .SRCINFO | grep -E '^\s*url\s*=' | choose 2 | xargs -0)
+	desc=$(cat .SRCINFO | grep -E '^\s*pkgdesc\s*=' | head -n 1 | choose -f '=' 1 | xargs -0)
+	ver=$(cat .SRCINFO | grep -E '^\s*pkgver\s*=' | choose 2 | xargs -0)
 
-  echo -e "| [$name]($url) | $desc | $ver |"
+	echo -e "| [$name]($url) | $desc | $ver |"
 
 done
