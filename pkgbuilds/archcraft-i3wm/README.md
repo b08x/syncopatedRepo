3ab64746109c47d5efb5387104b9b4916735c0bf
b08x <rwpannick@gmail.com>
2024-02-16 20:03:40 -0500

    updates

---
+++ pkgbuilds/archcraft-i3wm/README.md
@@ -0,0 +1,1 @@
+**Documentation** : [I3wm](https://wiki.archcraft.io/docs/window-managers/tiling-wm/i3wm)
--- tools/build.sh
+++ tools/build.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
-declare -rx timestampe=$(date +%Y%m%d%H%M)
+##########################################################################
+#                                                                        #
+#                   ___  _     _ _   _      _                           #
+#                  / _ \| |__ (_) |_| |__  (_) ___                      #
+#                 | | | | '_ \| | __| '_ \ | |/ _ \                     #
+#                 | |_| | |_) | | |_| | | || |  __/                     #
+#                  \___/|_.__// |\__|_| |_|/ |\___|                     #
+#                          |__/          |__/                           #
+#                                                                        #
+#   This script automates the process of building packages for the      #
+#   Arch Linux distribution. It handles updating package                #
+#   checksums, generating the .SRCINFO file, building packages for      #
+#   multiple architectures, and syncing the local repository with       #
+#   remote hosts.                                                       #
+#                                                                        #
+##########################################################################
 
-#########################################################################
-#                             set colors                                 #
-#########################################################################
-
+# Set colors
 declare -rx ALL_OFF="\e[1;0m"
 declare -rx BBOLD="\e[1;1m"
 declare -rx BLUE="${BBOLD}\e[1;34m"
@@ -14,6 +26,7 @@ declare -rx GREEN="${BBOLD}\e[1;32m"
 declare -rx RED="${BBOLD}\e[1;31m"
 declare -rx YELLOW="${BBOLD}\e[1;33m"
 
+# Function to print styled text
 say() {
 	local statement=$1
 	local color=$2
@@ -21,37 +34,61 @@ say() {
 	echo -e "${color}${statement}${ALL_OFF}"
 }
 
-#########################################################################
-#                             set locations                             #
-#########################################################################
-declare -rx SOUDNDBOT="/mnt/soundbot"
-declare -rx BUILDS="/srv/builds"
+##########################################################################
+#                                                                        #
+#   Declare variables for directories and settings used in the script.   #
+#                                                                        #
+##########################################################################
 
-declare -rx WORKSPACE="${SOUDNDBOT}/Workspace/syncopatedRepo"
+# Define the workspace and package build directories
+declare -rx WORKSPACE="/mnt/soundbot/Workspace/syncopatedRepo"
 declare -rx PKGBUILDS="${WORKSPACE}/pkgbuilds"
-
-declare -rx REPOSITORY_LOCAL="$BUILDS/repository"
 declare -rx REPOCTL_CONFIG="${WORKSPACE}/repoctl/config.toml"
 
-declare -rx MIRRORS=("bender.syncopated.net" "ec2-user@syncopated.hopto.org")
-
-declare -rx SRCDEST=$BUILDS/packages/sources
-
+declare -rx BUILDS="/srv/builds"
+declare -rx REPOSITORY_LOCAL="$BUILDS/repository"
+declare -rx SRCDEST="${BUILDS}/packages/sources"
 declare -rx CHROOT="/mnt/chroots/arch"
 
+# Other settings
 declare -rx PACKAGER="Robert Pannick <rwpannick@gmail.com>"
 declare -rx GPGKEY="DF7A6571781ACB52FA9CF8C1EB4DFE46828DFEDD"
-#########################################################################
-#                             set functions                             #
-#########################################################################
+
+# Mirror locations
+declare -rx MIRRORS=("bender.syncopated.net" "ec2-user@syncopated.hopto.org")
+
+# Debug mode
+if [[ ${1} == 'debug' ]]; then
+	declare -rx DEBUG="True"
+	say "Debug mode enabled." $RED
+fi
+
+##########################################################################
+#                                                                        #
+#   Unmount the chroot if it's mounted.                                  #
+#                                                                        #
+##########################################################################
+
 unmount_chroot() {
 	if mountpoint -q $CHROOT; then sudo umount $CHROOT; fi
 }
+
+##########################################################################
+#                                                                        #
+#   Create a fresh chroot filesystem using tmpfs                         #
+#                                                                        #
+##########################################################################
 
 mount_chroot() {
 	unmount_chroot
 	sudo mount --mkdir -t tmpfs -o defaults,size=8G tmpfs $CHROOT
 }
+
+##########################################################################
+#                                                                        #
+#   Set the makepkg and pacman configurations for building packages.     #
+#                                                                        #
+##########################################################################
 
 set_configs() {
 	sudo chown -R $USER:$USER /usr/share/devtools
@@ -60,32 +97,47 @@ set_configs() {
 	cp -f $REPOCTL_CONFIG $HOME/.config/repoctl/config.toml
 }
 
+##########################################################################
+#                                                                        #
+#   Clean up resources and exit gracefully.                              #
+#                                                                        #
+##########################################################################
+
 cleanup() {
-	echo "all set!"
-	# if [[ ! $DEBUG ]]; then
-	#   unmount_chroot
-	#   # rm -rf $SRCDEST && mkdir -p $SRCDEST
-	# fi
+	if [[ ! $DEBUG ]]; then
+	  unmount_chroot && echo "chroot umounted"
+	fi
+echo "All set!"
 }
+
+# Trap signals for cleanup
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+# Create necessary directories
+mkdir -pv $BUILDS/{packages/sources,repository}
+
+##########################################################################
+#                                                                        #
+#   Greet the user with a stylish message.                               #
+#                                                                        #
+##########################################################################
+
+say "Greetings!" $GREEN
+
+##########################################################################
+#                                                                        #
+#   Change directory to the Workspace folder and fetch the latest        #
+#   commits. Rsync makepkg and pacman configs to devtools directory.    #
+#   Change into the pkgbuild folder and run `fd` with gum filter to      #
+#   select packages for building.                                       #
+#                                                                        #
+##########################################################################
 
 cd $WORKSPACE && git fetch && git pull
 
-set_configs
-unmount_chroot
+set_configs && cd $PKGBUILDS
 
-#########################################################################
-#                             Greetings                                 #
-#########################################################################
-gum style \
-	--foreground 014 --border-foreground 024 --border double \
-	--align center --width 50 --margin "1 2" --padding "2 4" \
-	'Hello.' && sleep 1 && clear
-
-#########################################################################
-
-cd $PKGBUILDS
-
-say "select packages to build" $GREEN
+say "Select packages to build" $GREEN
 say "-------------------------" $GREEN
 
 if [[ "${1}" == 'all' ]]; then
@@ -96,27 +148,19 @@ else
 	package_selection=$(fd . -t d --max-depth=1 $PKGBUILDS -x echo -e {/} | gum filter --no-limit)
 fi
 
-say "${package_selection}" $BLUE
+say "${package_selection}" $BLUE && sleep 1
 
-#########################################################################
-trap cleanup SIGINT SIGTERM ERR EXIT
-
-if [[ ${3} == 'debug' ]]; then
-	declare -rx DEBUG="True"
-	say "Debug...\n" $RED
-fi
-
-#phrase=$(gum input --password)
-
-phrase=$(cat /tmp/pkgbuilding)
-
-mkdir -pv $SRCDEST
-mkdir -pv $BUILDS/{packages,repository}
+##########################################################################
+#                                                                        #
+#   Update package checksums and generate the .SRCINFO file, which       #
+#   contains metadata about the package, such as its name, version,      #
+#   description, and supported architectures. We then parse the          #
+#   supported architectures from the .SRCINFO file and assign them to    #
+#   the 'architectures' array for building.                              #
+#                                                                        #
+##########################################################################
 
 for pkgname in ${package_selection[@]}; do
-
-	mount_chroot
-
 	cd "${PKGBUILDS}/${pkgname}"
 
 	updpkgsums && makepkg --printsrcinfo >.SRCINFO
@@ -130,30 +174,37 @@ for pkgname in ${package_selection[@]}; do
 		architectures=("$srcinfo")
 	fi
 
+	##########################################################################
+	#                                                                        #
+	#   Build the package for each available architecture.                  #
+	#                                                                        #
+	##########################################################################
+
 	for arch in ${architectures[@]}; do
 
-		gum style \
-			--foreground 014 --border-foreground 024 --border double \
-			--align center --width 50 --margin "1 2" --padding "2 4" \
-			"Building ${pkgname}-${arch}" && sleep 1 && clear
+		say "Building ${pkgname}-${arch}" $YELLOW && sleep 1
 
-		declare -x PKGDEST=$BUILDS/packages/$arch
-		declare -x SRCPKGDEST=$BUILDS/packages/srcpackages/$arch
-		declare -x LOGDEST=$BUILDS/packages/makepkglogs/$arch
+		# Set package destinations
+		declare -x PKGDEST=$BUILDS/packages/$arch && mkdir -p $PKGDEST
+		declare -x SRCPKGDEST=$BUILDS/packages/srcpackages/$arch && mkdir -p $SRCPKGDEST
+		declare -x LOGDEST=$BUILDS/packages/makepkglogs/$arch && mkdir -p $LOGDEST
 
-		mkdir -p $PKGDEST
-		mkdir -p $SRCPKGDEST
-		mkdir -p $LOGDEST
+		# Mount chroot
+		mount_chroot
 
-		extra-${arch}-build -c -r $CHROOT -- -c -n -u
+		# Build the package
+		extra-${arch}-build -c -r $CHROOT -- -c -n -u || break
 
-		cd $PKGDEST
-
+		# Sign the package
 		for pkg in *.zst; do
-			echo "${phrase}" | gpg2 -v --batch --yes --detach-sign --pinentry-mode loopback --passphrase --passphrase-fd 0 --out $pkg.sig --sign $pkg
+			echo $(skate get advice) | gpg2 -v --batch \
+			--yes --detach-sign --pinentry-mode loopback \
+			--passphrase --passphrase-fd 0 \
+			--out $pkg.sig --sign $pkg && \
 			repoctl add -P $arch -m -r ./$pkg
 		done
 
+		# Add package to repository
 		cd $BUILDS/repository/$arch
 
 		if [[ $arch == 'x86_64' ]]; then
@@ -165,16 +216,20 @@ for pkgname in ${package_selection[@]}; do
 		repo-add -n -k $GPGKEY $repo_db *.pkg.tar.zst -s
 
 		cd "${PKGBUILDS}/${pkgname}"
-	done
+	done # end loop
+done # end loop
 
-done
+##########################################################################
+#                                                                        #
+#   If packages are successfully added to the repository database,       #
+#   then the local repository is synced with a local NAS and a remote    #
+#   VPS.                                                                 #
+#                                                                        #
+##########################################################################
 
 for mirror in ${MIRRORS[@]}; do
-	echo -e "syncing local repository to remote ${mirror}\n"
+	echo -e "Syncing local repository to remote ${mirror}\n"
 	rsync -avP --delete "${BUILDS}/repository/" "${mirror}:/usr/share/nginx/html/syncopated/repo/" || continue
 done
 
-gum style \
-	--foreground 014 --border-foreground 024 --border double \
-	--align center --width 50 --margin "1 2" --padding "2 4" \
-	'Enf of Program.' && sleep 1 && clear
+say "End of Program." $GREEN && sleep 1
